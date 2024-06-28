6f40e2ed612fd3a693c738a64b84878e98b6d8cb
LFSCamargo <luizepauloxd@gmail.com>
2024-06-28 17:53:50 -0300

    chore: code updates

---
+++ apps/server/scripts/seed-admin-production.script.ts
@@ -0,0 +1,39 @@
+import { connect, connection } from "mongoose";
+import { UserModel } from "../src/models";
+import { Env } from "../src/env";
+import { PasswordUtility } from "../src/utils";
+
+/**
+ * @description - This is used to seed the admin user into the database.
+ * @generates - A new admin user in the database with the following credentials:
+ * - email: atendimento@ipebank.com.br
+ * - password: ipe@bank@2024@admin
+ * - name: IpeBank Admin
+ */
+async function seed() {
+  console.log(Env);
+
+  await connect(Env.MONGO_URI, {
+    dbName: Env.DB_NAME,
+  });
+
+  const password = "ipe@bank@admin";
+
+  const payload = {
+    email: "johny.cunha@ipebank.com.br",
+    password: PasswordUtility.encryptPassword(password),
+    name: "IpeBank Admin",
+  };
+
+  const admin = new UserModel(payload);
+
+  await admin.save();
+
+  console.log("Admin user created successfully.");
+  console.log("Email:", admin.email);
+  console.log("Password:", password);
+
+  await connection.close();
+}
+
+seed();
