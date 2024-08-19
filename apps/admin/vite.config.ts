46decdb21be481274fa9edd3be56a49b16d0b306
LFSCamargo <luizepauloxd@gmail.com>
2024-08-19 00:44:50 -0300

    chore: adds deploy to app platform digital ocean

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -4,6 +4,9 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    port: 3001,
+  },
   plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
